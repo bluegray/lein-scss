4dc4daeb82a458648dc74d73b85a8c37d9e94854
Francois du Toit <bluegray@users.noreply.github.com>
2014-09-16 15:30:06 +0200

    Lots of fixes and refactor. Get executable args from project.clj

--- src/leiningen/scss.clj
+++ src/leiningen/scss.clj
@@ -38,7 +38,9 @@
   `(let [start# (. System (nanoTime))
          ret# ~expr]
      (lein/debug
-      (color :green (str "Elapsed time: " (/ (double (- (. System (nanoTime)) start#)) 1000000.0) " msecs")))
+      (color :green (str "Elapsed time: "
+                         (/ (double (- (. System (nanoTime)) start#)) 1000000.0)
+                         " msecs")))
      ret#))
 
 (defn color
@@ -57,20 +59,41 @@
   [file]
   (re-find #"^.+\.scss$" (.getName file)))
 
-(defn files-with-partial
-  [partial project args]
-  (lein/debug (color :blue "Getting files-with-partial" partial))
-  (for [file (file-seq (io/file (source-dir project args)))
-        :let [pattern (re-pattern (str "@import.*" partial))]
-        :when (and (.isFile file)
-                   (some #(re-find pattern %) (line-seq (io/reader file))))]
-    file))
+(defn ignore?
+  [file]
+  (re-find #"\.sass-cache" (.getPath file)))
 
-(defn files-in-source
+(defn contains-partial?
+  [partial-name file]
+  (when-not
+      (re-find (re-pattern partial-name) (.getName file))
+      (some #(re-find (re-pattern (str "@import\\s+['\"].*\\b" partial-name "['\"];")) %)
+            (line-seq (io/reader file)))))
+
+(defn files-in-source-dir
   [project args]
-  (let [result (print-time (filter #(.isFile %) (file-seq (io/file (source-dir project args)))))]
-    (lein/debug (color :blue "files-in-source" (string/join " | " result)))
-    (->> result (filter is-scss?) (remove is-scss-partial?))))
+  (filter #(.isFile %) (file-seq (io/file (source-dir project args)))))
+
+(defn files-with-partial
+  [partial-name project args]
+  (lein/debug (color :blue "Searching for files with partial" partial-name))
+  (let [files   (print-time (files-in-source-dir project args))
+        result  (->> files
+                     (filter is-scss?)
+                     (remove ignore?)
+                     (filter  (partial contains-partial? partial-name)))]
+    (lein/debug (color :blue "Found:" (string/join " | " result)))
+    result))
+
+(defn stylesheets-in-source
+  [project args]
+  (let [files  (print-time (files-in-source-dir project args))
+        result (->> files
+                    (filter is-scss?)
+                    (remove is-scss-partial?))]
+    (lein/debug (color :blue "Stylesheets found in" (source-dir project args) ":"
+                       (string/join " | " result)))
+    result))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main
@@ -84,10 +107,25 @@
         css-file  (string/replace dest-file #"\.scss$" ".css")
         from      filename
         to        (str dest css-file)
-        cmd       [(-> project :scss :executable (or "sass")) from to]]
-        (lein/info (color :bright-blue "Converting" from "->" to))
-        (io/make-parents to)
-        (print-time (apply eval/sh cmd))))
+        exec      (-> project :scss :executable (or "sass"))
+        args      (-> project :scss :args)
+        cmd       (conj
+                   (cond-> [exec]
+                           (not (empty? args))
+                           (as-> args-list (apply conj args-list args)))
+                   from to)]
+    (lein/info (color :bright-blue "Converting" from "->" to))
+    (lein/debug (color :blue "cmd: " (string/join " " cmd)))
+    (io/make-parents to)
+    (print-time (apply eval/sh cmd))))
+
+(defn handle-conversion
+  [file project args]
+  (if (is-scss-partial? file)
+    (doall
+     (map #(handle-conversion % project args)
+          (print-time (files-with-partial (last (re-find #"_([^.]+)" (.getName file))) project args))))
+    (convert file project args)))
 
 (defn handle-change
   [project args {:keys [file count action]}]
@@ -95,11 +133,8 @@
     (lein/debug (color :blue "Detected file change: [file count action]"
                        file count action))
     (when-let [file (and (is-scss? file) file)]
-      (if (is-scss-partial? file)
-        (doall
-         (map #(convert % project args)
-              (print-time (files-with-partial (re-find #"[^_.]+" (.getName file)) project args))))
-        (convert file project args)))
+      (lein/info (color :bright-yellow "Detected file change:" (.getName file)))
+      (handle-conversion file project args))
     (catch Exception e (lein/warn e))))
 
 (defn watchd
@@ -110,16 +145,20 @@
 (defn scss
   "Watch a dir for changes and compile css"
   [project & args]
+  (lein/debug (color :blue "args: " args))
   (if-let [dir (source-dir project args)]
     (if (.isDirectory (io/as-file dir))
       (do
-        (print-time (doall (map #(convert % project args) (print-time (files-in-source project args)))))
-        (let [watcher (watchd project args dir)]
-          (try
-            (loop [state* (reset! state "started")]
-              (Thread/sleep 5000)
-              (recur (reset! state "running")))
-            (catch Exception e (lein/warn e))
-            (finally (close-watcher watcher)))))
+        (when-not (some #{"auto"} args)
+          (print-time (doall (map #(convert % project args)
+                                  (print-time (stylesheets-in-source project args))))))
+        (when-not (some #{"once"} args)
+            (let [watcher (watchd project args dir)]
+              (try
+                (loop [state* (reset! state "started")]
+                  (Thread/sleep 5000)
+                  (recur (reset! state "running")))
+                (catch Exception e (lein/warn e))
+                (finally (close-watcher watcher))))))
       (lein/warn dir "is not a directory"))
     (lein/warn "Please provide a directory to watch")))
