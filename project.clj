37a461c8529ae72eaf61999824208c250d438fba
Francois du Toit <bluegray@users.noreply.github.com>
2014-09-24 18:02:57 +0200

    Add multiple builds support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-scss "0.1.1"
+(defproject lein-scss "0.1.2-SNAPSHOT"
   :description "A lein plugin to compile scss to css."
   :url "https://github.com/bluegray/lein-scss"
   :scm {:name "git"
@@ -6,6 +6,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :signing {:gpg-key "bluegray"}
-  :deploy-repositories [["clojars" {:creds :gpg}]]
+  :deploy-repositories [["releases" :clojars {:creds :gpg}]]
   :eval-in-leiningen true
   :dependencies [[juxt/dirwatch "0.2.0"]])
--- src/leiningen/scss.clj
+++ src/leiningen/scss.clj
@@ -18,18 +18,25 @@
    :white   "\u001b[37m" :bright-white   "\u001b[1m\u001b[37m"
    :default "\u001b[39m"})
 
-(def state (atom "stopped"))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Config
 
-(defn source-dir
+(defn get-build-map
+  [project build-key]
+  (-> project :scss :builds build-key))
+
+(defn get-arg-builds
   [project args]
-  (or (-> project :scss :source-dir) (first args) "scss/"))
+  (filter #(some #{%} (-> project :scss :builds keys))
+          (map read-string args)))
+
+(defn source-dir
+  [build-map]
+  (or (:source-dir build-map) "scss/"))
 
 (defn dest-dir
-  [project args]
-  (or (-> project :scss :dest-dir) (second args) "css/"))
+  [build-map]
+  (or (:dest-dir build-map) "css/"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Helpers
@@ -68,86 +75,86 @@
   (re-find #"\.sass-cache" (.getPath file)))
 
 (defn files-in-source-dir
-  [project args]
-  (filter #(.isFile %) (file-seq (io/file (source-dir project args)))))
+  [build-map]
+  (filter #(.isFile %) (file-seq (io/file (source-dir build-map)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Partial helpers
 
 (defn stylesheets-in-source
   "Get all stylesheets in source dir, not including partials"
-  [project args]
-  (let [files  (print-time (files-in-source-dir project args))
+  [build-map]
+  (let [files  (print-time (files-in-source-dir build-map))
         result (->> files
                     (filter is-scss?)
                     (remove ignore?)
                     (remove is-scss-partial?))]
-    (lein/debug (color :blue "Stylesheets found in" (source-dir project args) ":"
+    (lein/debug (color :blue "Stylesheets found in" (source-dir build-map) ":"
                        (string/join " | " result)))
     result))
 
 (defn get-partial-file-from-str*
   "Takes a partial string and return the actual file."
-  [s project args]
-  (let [m (for [f (remove ignore? (files-in-source-dir project args))]
+  [build-map partial-str]
+  (let [m (for [f (remove ignore? (files-in-source-dir build-map))]
             [f (-> f .getPath
                    (string/replace #"/_" "/")
                    (string/replace #".scss$" "")
-                   (string/replace (re-pattern (str (source-dir project args) "/")) ""))])]
-    (some (fn [f] (when (re-find (re-pattern s) (second f)) (first f))) m)))
+                   (string/replace (re-pattern (str (source-dir build-map) "/")) ""))])]
+    (some (fn [f] (when (re-find (re-pattern partial-str) (second f)) (first f))) m)))
 (def get-partial-file-from-str (memoize get-partial-file-from-str*))
 
 (defn deps-in-scss
   "Get all direct dependencies for an .scss file"
-  [file project args]
+  [build-map file]
   (with-open [rdr (io/reader file)]
     (->> (some->> rdr
                   line-seq
                   (map #(re-find #"@import\s+['\"](.*)['\"]" %))
                   (remove nil?)
                   (map last)
-                  (map #(get-partial-file-from-str % project args)))
+                  (map (partial get-partial-file-from-str build-map)))
          (remove nil?)
          doall)))
 
 (defn all-deps-for-scss
   "Get all dependencies for an .scss file, dependencies of partials too."
-  [file project args]
+  [build-map file]
   (let [children (atom [])
         result   (distinct
                   (tree-seq (fn [child]
                               (let [get? (not (some #{child} @children))]
                                 (swap! children conj child)
                                 get?))
-                            #(deps-in-scss % project args) file))]
+                            (partial deps-in-scss build-map) file))]
     [(abs (first result)) (map abs (rest result))]))
 
 (defn source-tree
-  [project args]
-  (pmap #(all-deps-for-scss %  project args) (stylesheets-in-source project args)))
+  [build-map]
+  (pmap (partial all-deps-for-scss build-map) (stylesheets-in-source build-map)))
 
 (defn stylesheets-with-partial
-  [file project args]
-  (print-time (pmap first (filter #(some #{(abs file)} (second %)) (source-tree project args)))))
+  [build-map file]
+  (print-time (pmap first (filter #(some #{(abs file)} (second %)) (source-tree build-map)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main
 
 (defn convert
-  [file project args]
+  [build-map file]
   (let [filename  (abs file)
-        source    (abs (source-dir project args))
-        dest      (abs (dest-dir project args))
+        source    (abs (source-dir build-map))
+        dest      (abs (dest-dir build-map))
         dest-file (string/replace filename (re-pattern (str "^" source)) "")
         css-file  (string/replace dest-file #"\.scss$" ".css")
         from      filename
         to        (str dest css-file)
-        exec      (-> project :scss :executable (or "sass"))
-        args      (-> project :scss :args)
+        exec      (-> build-map :executable (or "sass"))
+        cmd-args  (:args build-map)
         cmd       (conj
                    (cond-> [exec]
-                           (not (empty? args))
-                           (as-> args-list (apply conj args-list args)))
+                           (not (empty? cmd-args))
+                           (as-> args-list (apply conj args-list cmd-args)))
                    from to)]
     (lein/info (color :bright-blue "Converting" from "->" to))
     (lein/debug (color :blue "cmd: " (string/join " " cmd)))
@@ -155,45 +162,59 @@
     (print-time (apply eval/sh cmd))))
 
 (defn handle-conversion
-  [file project args]
+  [build-map file]
   (if (is-scss-partial? file)
     (doall
-     (pmap #(handle-conversion % project args)
-           (print-time (stylesheets-with-partial file project args))))
-    (convert file project args)))
+     (pmap (partial handle-conversion build-map)
+           (print-time (stylesheets-with-partial build-map file))))
+    (convert build-map file)))
 
 (defn handle-change
-  [project args {:keys [file count action]}]
+  [build-map {:keys [file count action]}]
   (try
     (lein/debug (color :blue "Detected file change: [file count action]"
                        file count action))
     (when-let [file (and (is-scss? file) file)]
       (lein/info (color :bright-yellow "Detected file change:" (.getName file)))
-      (handle-conversion file project args))
+      (handle-conversion build-map file))
     (catch Exception e (lein/warn (with-out-str (clojure.repl/pst e 20))))))
 
 (defn watchd
-  [project args dir]
+  [build-map dir]
   (lein/info (color :bright-yellow "Watching directory" dir))
-  (watch-dir (partial handle-change project args) (io/file dir)))
+  (watch-dir (partial handle-change build-map) (io/file dir)))
+
+(def watchers (atom []))
+
+(defn auto
+  [project args builds]
+  (doseq [build builds
+          :let [build-map (get-build-map project build)]]
+    (swap! watchers conj (watchd build-map (source-dir build-map))))
+  (.addShutdownHook
+   (Runtime/getRuntime)
+   (Thread. #(do (println (color :bright-magenta "\nGoodbye!"))
+                 (doseq [watcher @watchers] (close-watcher watcher)))))
+  (while true
+    (lein/debug (color :blue "Watchers: " @watchers))
+    (Thread/sleep 20000)))
+
+(defn once
+  [project args builds]
+  (doseq [build builds
+          :let [build-map (get-build-map project build)]]
+    (-> (pmap (partial convert build-map)
+              (print-time (stylesheets-in-source build-map)))
+        doall
+        print-time)))
 
 (defn scss
-  "Compile all stylesheets in the source dir and optionally watch for changes."
+  "Compile all stylesheets in the source dir and/or watch for changes."
   [project & args]
   (lein/debug (color :blue "args: " args))
-  (if-let [dir (source-dir project args)]
-    (if (.isDirectory (io/as-file dir))
-      (do
-        (when-not (some #{"auto"} args)
-          (print-time (doall (pmap #(convert % project args)
-                                  (print-time (stylesheets-in-source project args))))))
-        (when-not (some #{"once"} args)
-            (let [watcher (watchd project args dir)]
-              (try
-                (loop [state* (reset! state "started")]
-                  (Thread/sleep 5000)
-                  (recur (reset! state "running")))
-                (catch Exception e (lein/warn (with-out-str (clojure.repl/pst e 20))))
-                (finally (close-watcher watcher))))))
-      (lein/warn dir "is not a directory"))
-    (lein/warn "Please provide a directory to watch")))
+  (let [builds (get-arg-builds project args)]
+    (do
+      (when-not (some #{"auto"} args)
+        (once project args builds))
+      (when-not (some #{"once"} args)
+        (auto project args builds)))))
