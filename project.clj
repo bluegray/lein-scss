cbed81e86949c756c5a31ee191cce5bbd502d4a0
Francois du Toit <bluegray@users.noreply.github.com>
2015-02-19 11:08:41 +0200

    Version 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-scss "0.2.3-SNAPSHOT"
+(defproject lein-scss "0.2.3"
   :description "A lein plugin to compile scss to css."
   :url "https://github.com/bluegray/lein-scss"
   :scm {:name "git"
