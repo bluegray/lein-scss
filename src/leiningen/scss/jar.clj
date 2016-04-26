c2037fccda8d0887f3109ec80f156928cde3d85f
Howard P. Logsdon <howard@hplogsdon.com>
2016-04-25 20:15:19 -0600

    Add hooks to enable compiling scss into jar/uberjar
    Leiningen has hook functionality that enable plugins to tie into the
    existing tasks of lein. lein-scss seems to be the only scss plugin that does
    not leverage this, which is unfortunate since it seems to be the only one
    that can use the native libsass library for compilation. This commit should
    provide the neccessary hook functions that will expose both compile and jar
    tasks to leiningen so that a single call to `lein compile` will output scss
    as the project is configured, and calling `lein jar` or `lein uberjar` will
    enable adding compiled scss as css to the resulting jarfile.
    
    In order to better suit users, I've added a :jar keyword flag to the build
    profile of the scss configuration. Setting this to false will prevent the
    process from adding the css into the jarfile.
    
    https://github.com/technomancy/leiningen/blob/master/doc/PLUGINS.md#hooks

---
+++ src/leiningen/scss/jar.clj
@@ -0,0 +1,34 @@
+(ns leiningen.scss.jar
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]
+            [leiningen.scss.config :as config]))
+
+(defn relative-path [parent child]
+  (let [relative (s/replace child parent "")]
+    (when (= child relative)
+      (throw (Exception. (str child " is not a child of " parent))))
+    (s/replace relative #"^[\\/]" "")))
+
+(defn file-bytes [file]
+  (with-open [input (java.io.FileInputStream. file)]
+    (let [data (byte-array (.length file))]
+      (.read input data)
+      data)))
+
+(defn canonical-path [path]
+  (.getCanonicalPath (io/file path)))
+
+(defn get-files [path]
+  (filter #(not (.isDirectory %)) (file-seq (io/file path))))
+
+(defn filespecs [project]
+  (let [builds (-> project :scss :builds)
+        build-maps (apply dissoc builds (keep #(-> % val (contains? :jar) (if nil (key %))) builds))
+        paths (dedupe (map #(config/dest-dir (val %)) build-maps))]
+    (flatten
+      (for [path paths]
+        (for [file (get-files path)]
+          {:type :bytes
+           :path (relative-path (canonical-path path) (canonical-path file))
+           :bytes (file-bytes file)})))))
+
